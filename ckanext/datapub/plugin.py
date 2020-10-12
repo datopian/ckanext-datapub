14a299ece2413a33893be6d188efaf3ec1a5598e
Anuar Ustayev <anuar.ustayev@gmail.com>
2020-10-12 16:13:36 +0600

    Add missing helpers.

--- ckanext/datapub/plugin.py
+++ ckanext/datapub/plugin.py
@@ -1,9 +1,11 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from . import helpers
 
 
 class DatapubPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.ITemplateHelpers)
 
     # IConfigurer
 
@@ -11,3 +13,10 @@ class DatapubPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'datapub')
+
+    # ITemplateHelpers
+
+    def get_helpers(self):
+        return {
+                'extstorage_lfs_url': helpers.lfs_url,
+                'extstorage_organization_name': helpers.organization_name}
