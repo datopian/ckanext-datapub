c94d8181fd532c37f2521d571476cf979360e326
michael beilin <michael.beilin@datopian.com>
2020-10-27 07:47:20 +0200

    [datapub-nhs]: add datapub-nhs implementation

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
-git clone git@github.com:datopian/datapub.git
-cd datapub
+git clone git@github.com:datopian/datapub-nhs.git
+cd datapub-nhs
 yarn
 yarn build
 cd -
 rm -r ckanext/datapub/fanstatic/**/*
-cp -r datapub/build/static/* ckanext/datapub/fanstatic/
+cp -r datapub-nhs/build/static/* ckanext/datapub/fanstatic/
