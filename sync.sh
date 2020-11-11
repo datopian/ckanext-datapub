cfe33390dadd0d75cd47c6245d39b4eea4fd4d08
zelima <ir.mchedlishvili@gmail.com>
2020-11-11 16:32:22 +0400

    Accept argiment from sync

--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
-DATAPUB_APP=${DATAPUB_APP:='https://github.com/datopian/datapub'}
-git clone $DATAPUB_APP
-cd datapub-gdx && npm install . && npm run build
+DATAPUB_APP=${1:='https://github.com/datopian/datapub'}
+git clone $DATAPUB_APP datapub
+cd datapub && npm install . && npm run build
 wget https://raw.githubusercontent.com/johanhaleby/bash-templater/master/templater.sh
 for x in $(ls build/static/js/*.js build/static/css/*.css); do
   bundles=$bundles"\{\% resource $x \%\}"\\n
