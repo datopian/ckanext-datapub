8f67576c381b6a84eb1e07e1006848c765797243
amercader <amercadero@gmail.com>
2020-12-16 15:57:53 +0100

    Improve sync script
    Check if directories / files exist, ignore files in repo

--- sync.sh
+++ sync.sh
@@ -1,8 +1,22 @@
-DATAPUB_APP=${1:='https://github.com/datopian/datapub'}
-DATAPUB_VERSION=${2:='master'}
-git clone --branch $DATAPUB_VERSION $DATAPUB_APP datapub
-wget https://raw.githubusercontent.com/johanhaleby/bash-templater/master/templater.sh
-cd datapub && npm install . && npm run build
+#!/bin/bash
+
+DATAPUB_APP=datapub-gdx
+DATAPUB_REPO=https://github.com/datopian/$DATAPUB_APP
+DATAPUB_VERSION=v0.0.26
+
+if [ ! -d datapub-gdx ]; then
+    git clone --branch $DATAPUB_VERSION $DATAPUB_APP datapub-gdx
+else
+    cd datapub-gdx
+    git fetch
+    git checkout $DATAPUB_VERSION
+    cd -
+fi
+
+if [ ! -f templater.sh ]; then
+    wget https://raw.githubusercontent.com/johanhaleby/bash-templater/master/templater.sh
+fi
+cd datapub-gdx && npm install . && npm run build
 for x in $(ls build/static/js/*.js build/static/css/*.css); do
   bundles=$bundles"\{\% resource \"${x}\" \%\}"\\n
 done
