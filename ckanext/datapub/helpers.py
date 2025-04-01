5fe090efde8780822054211b2129bc4e02dde7e2
Luccas Mateus <luccasmmg@gmail.com>
2025-04-01 19:37:56 -0300

    Upgrade to CKAN 2.11

--- ckanext/datapub/helpers.py
+++ ckanext/datapub/helpers.py
@@ -1,13 +1,14 @@
 """Template helpers for ckanext-datapub
 """
 import ckan.plugins.toolkit as toolkit
+import logging
 
-SERVER_URL_CONF_KEY = 'ckanext.external_storage.storage_service_url'
+SERVER_URL_CONF_KEY = 'ckanext.blob_storage.storage_service_url'
 
+log = logging.getLogger(__name__)
 
 def lfs_url():
     return toolkit.config.get(SERVER_URL_CONF_KEY)
-
 
 def organization_name(package_name=None):
     if package_name:
@@ -28,3 +29,26 @@ def organization_name(package_name=None):
             return '_'
     else:
         return '_'
+
+def get_user_with_token(user):
+    user = user.as_dict()
+    context = {'ignore_auth': True}
+    try:
+        api_tokens = toolkit.get_action('api_token_list')(
+            context, {'user_id': user['name']}
+        )
+
+        for token in api_tokens:
+            if token['name'] == 'datapub_token':
+                toolkit.get_action('api_token_revoke')(context, {'jti': token['id']})
+
+        datapub_token = toolkit.get_action('api_token_create')(
+            context, {'user': user['name'], 'name': 'datapub_token'}
+        )
+
+        return datapub_token.get('token')
+
+    except Exception as e:
+        log.error('Failed to generate frontend token')
+        log.error(e)
+
--- ckanext/datapub/plugin.py
+++ ckanext/datapub/plugin.py
@@ -1,7 +1,7 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+import ckan.logic.action as action
 from . import helpers
-
 
 class DatapubPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
@@ -12,11 +12,14 @@ class DatapubPlugin(plugins.SingletonPlugin):
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic', 'datapub')
+        toolkit.add_resource("assets", "datapub")
 
     # ITemplateHelpers
 
     def get_helpers(self):
         return {
                 'extstorage_lfs_url': helpers.lfs_url,
-                'extstorage_organization_name': helpers.organization_name}
+                'extstorage_organization_name': helpers.organization_name,
+                'get_user_with_token': helpers.get_user_with_token
+    }
+
